14e9b4c66073c687572d2c9f893594dab81621f6
Matt Daly <matt.tyler.daly@gmail.com>
2022-05-13 01:31:00 -0400

    Initial Commit attempt

---
+++ src/router/routes.js
@@ -0,0 +1,28 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      // Home Page
+      {
+        path: "/",
+        name: "home",
+        component: () => import("pages/HomePage.vue"),
+      },
+    ],
+  },
+  // DataUtil
+  {
+    path: "/datautil",
+    name: "datautil",
+    component: () => import("components/DataUtil.vue"),
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+export default routes;
